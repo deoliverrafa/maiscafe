a6cc59ee633ca00fbdf37b174f37a591b9e1f4b5
deoliverrafa <127500319+deoliverrafa@users.noreply.github.com>
2023-04-20 16:43:21 -0300

    Add files via upload

--- cafe.js
+++ cafe.js
@@ -1,8 +1,11 @@
-const hamburger = document.querySelector('.toogle-btn');
-const hamburgerIcon = document.querySelector('.toogle-btn i');
+const hamburger = document.querySelector('.toogle-btn .fa-solid.fa-bars');
 const dropdownmenu = document.querySelector('.dropdown_menu');
-const navbar = document.querySelector('.navbar');
+const closemenu = document.querySelector('.closemenu')
 
 hamburger.onclick = function (){
     dropdownmenu.classList.toggle('active')
+}
+
+closemenu.onclick = function (){
+    dropdownmenu.classList.remove('active')
 }
